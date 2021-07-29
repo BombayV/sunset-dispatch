009b70a854c283a7ce8ed4bbac132c5d13bd09ad
XiosBombay <OMGmsebastian@gmail.com>
2021-07-28 23:16:35 -0500

    feat(ui): added call design

--- html/js/nui.js
+++ html/js/nui.js
@@ -1,4 +1,6 @@
 const doc = document
+const before = doc.getElementById('before')
+const after = doc.getElementById('after')
 
 function openTab(target, className, settings) {
     let i, tabcontent;
@@ -35,3 +37,36 @@ sel.addEventListener('change', function(e) {
         break;
     }
 }, false);
+
+let currentSlide = 0;
+let i;
+
+const slides = doc.getElementsByClassName("container-slides");
+
+after.addEventListener("click", () => {
+    console.log('b')
+    currentSlide++
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    if (currentSlide > slides.length) {
+      currentSlide = 1
+    }
+    slides[currentSlide - 1].style.display = "block";
+    slides[currentSlide - 1].style.animation = "fadeIn 1.5s linear";
+    doc.getElementById('num-slides').textContent = `${currentSlide} /${slides.length}`
+});
+  
+before.addEventListener("click", () => {
+    console.log('a')
+    currentSlide--
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    if (currentSlide < 1) {
+      currentSlide = slides.length
+    }
+    slides[currentSlide - 1].style.display = "block";
+    slides[currentSlide - 1].style.animation = "fadeIn 1.5s linear";
+    doc.getElementById('num-slides').textContent = `${currentSlide} /${slides.length}`
+});
---
+++ html/js/script.js
@@ -0,0 +1,7 @@
+const mainCont = doc.getElementById('main-container')
+
+doc.getElementById('test').addEventListener('click', () => {
+    const cont = doc.createElement('div')
+
+    cont.classList.add('container-slides');
+})
