97bc9992a39efcb6d7345a41b0f1ca90990f7efb
XiosBombay <OMGmsebastian@gmail.com>
2021-08-05 15:35:46 -0500

    feat(client): Added command suggestions

--- client/events_cl.lua
+++ client/events_cl.lua
@@ -14,7 +14,7 @@ CreateThread(function()
 	ESX.PlayerData = ESX.GetPlayerData()
 
 	TriggerEvent('chat:addSuggestion', '/forzar', 'Envia un msg a la policia de robo de vehiculo', {})
-	TriggerEvent('chat:addSuggestion', '/command', 'Envia un msg de entorno', {
+	TriggerEvent('chat:addSuggestion', '/entorno', 'Envia un msg de entorno', {
 		{ name="mensaje", help="Descripcion de lo occurido" }
 	})
 end)
